73f45e71bdf91fb141abb573bdd18c99c9864a35
Mr. Metelus <Mdmetelus@gmail.com>
2018-09-23 23:26:58 -0400

    Random
    Random  Web Dev Code from 
    Sun 15 Jul 2018 09∶12∶50 PM EDT

---
+++ Javascript_projects/First_js_code.js
@@ -0,0 +1,13 @@
+let example = 'Dylan'
+
+console.log(example);
+
+let name ="dylan";
+
+console.log(typeof name)
+
+let firstName = 'Dylan';
+let lastName = "Israel";
+
+consloe.log (firstName +' ' + lastName);
+console.log(`${firstname}`)
